b3f3f93eaff96c80fcb2d7b689c24cc200f2e0ef
@Taras-prog-byte <tkropyvnitskyy@gmail.com>
2021-11-15 20:31:30 +0200

    First-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductBinComponent } from './product-bin/product-bin.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductBinComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
